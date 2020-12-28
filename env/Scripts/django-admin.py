bc7ec81ed5500d5220179bfa8d3a44c1a593cafa
WataruMiyabayashi <68964721+WataruMiyabayashi@users.noreply.github.com>
2020-12-29 03:47:20 +0900

    Add files via upload

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\desktop\flask2\django-csv-upload-example\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
